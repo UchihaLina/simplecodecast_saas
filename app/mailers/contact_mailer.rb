61199b76e4e2785bc21d69a371c2f9bee571bcb4
Patricia <yuko.lina@gmail.com>
2015-05-30 01:09:55 +0000

    ops daisy, copied and pasted

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,11 +1,9 @@
 class ContactMailer < ActionMailer::Base
   default to: 'yuko.lina@gmail.com'
-
-  def contact_email(name, email, boody)
+  def contact_email(name, email, body)
     @name = name
     @email = email
     @body = body
-    
-    mail(from: email, subject: 'Contact From Message')
+    mail(from: email, subject: 'Contact Form Message')
   end
 end
