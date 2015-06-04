8752a47aa10a7f888a928a220f64bc57a979deeb
Patricia <yuko.lina@gmail.com>
2015-06-04 21:47:15 +0000

    added profiles new action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,3 +1,8 @@
 class ProfilesController < ApplicationController
-  
+  def new
+    # form where a user can fill out their own profile.
+    @user = User.find(params[:user_id]) #this can find which user in logged in
+    @profile = @user.build_profile #because we have nested on the routes we can user build_profile for this particular user, needed to create profiles folder at view and new.html.erb
+    @variable = params[:hello]
+  end
 end
