5eb97ac14cb46f2ee7fa7aba11fed7eea36ce82d
Patricia <yuko.lina@gmail.com>
2015-06-06 14:29:48 +0000

    changed new action in profiles control

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,7 +2,8 @@ class ProfilesController < ApplicationController
   def new
     # form where a user can fill out their own profile.
     @user = User.find(params[:user_id]) #this can find which user in logged in
-    @profile = @user.build_profile #because we have nested on the routes we can user build_profile for this particular user, needed to create profiles folder at view and new.html.erb
+    #@profile = @user.build_profile #because we have nested on the routes we can user build_profile for this particular user, needed to create profiles folder at view and new.html.erb
+    @profile = Profile.new # the previous will unlike the profile that was created before. That's why we use this line instead.
   end
   def create
     @user = User.find(params[:user_id]) #Over here i indicate that i want to create a profile to THIS specific logged in user
