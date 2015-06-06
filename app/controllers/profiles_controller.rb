18ee87fb78e6e689db3abae9ef64eea454fc6999
Patricia <yuko.lina@gmail.com>
2015-06-06 21:32:17 +0000

    added file fields for uploads and profiles

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,43 +3,39 @@ class ProfilesController < ApplicationController
   before_action :only_current_user
   def new
     # form where a user can fill out their own profile.
-    @user = User.find(params[:user_id]) #this can find which user in logged in
-    #@profile = @user.build_profile #because we have nested on the routes we can user build_profile for this particular user, needed to create profiles folder at view and new.html.erb
-    @profile = Profile.new # the previous will unlike the profile that was created before. That's why we use this line instead.
+    @user = User.find( params[:user_id] )
+    @profile = Profile.new
   end
-  def create
-    @user = User.find(params[:user_id]) #Over here i indicate that i want to create a profile to THIS specific logged in user
-    @profile = @user.build_profile(profile_params) #this time I'm passing parameters on the parenthesis to profile, dont forget to white list params
-    if @profile.save #If i can save
+  def create 
+    @user = User.find( params[:user_id] )
+    @profile = @user.build_profile(profile_params)
+    if @profile.save
       flash[:success] = "Profile Updated!"
-      redirect_to user_path(params[:user_id])
+      redirect_to user_path( params[:user_id] )
     else
       render action: :new
     end
   end
   def edit
-    @user = User.find(params[:user_id])
-    @profile = @user.profile #pulling the contact with the information added previously
+    @user = User.find( params[:user_id] )
+    @profile = @user.profile
   end
   def update
-    @user = User.find(params[:user_id]) #getting the user
-    @profile = @user.profile #using the profile from the user i got
-    if @profile.update_attributes(profile_params) #i already white listed this
-      flash[:success] = "Profile Updated."
-      redirect_to user_path(params[:user_id])
+    @user = User.find( params[:user_id] )
+    @profile = @user.profile
+    if @profile.update_attributes(profile_params)
+      flash[:success] = "Profile Updated!"
+      redirect_to user_path( params[:user_id] )
     else
-      flash[:danger] = "Error"
       render action: :edit
     end
   end
   private
-  def profile_params
-    #This method name has to match the parenthesis above
-    #This is the white list
-    params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
-  end
-  def only_current_user
-    @user = User.find(params[:user_id])
-    redirect_to(root_url) unless @user == current_user
-  end
+    def profile_params
+      params.require(:profile).permit(:first_name, :last_name,:avatar, :job_title, :phone_number, :contact_email, :description)
+    end
+    def only_current_user
+      @user = User.find( params[:user_id] )
+      redirect_to(root_url) unless @user == current_user
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  before_action :only_current_user
   def show
     @user = User.find(params[:id])
   end
